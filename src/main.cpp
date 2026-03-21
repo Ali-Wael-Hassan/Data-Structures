fd0f9a60776af09e0bc855d686428ec808d88453
Ali Hassan <aliwael01243@gmail.com>
2026-03-21 14:59:26 +0200

    added operator + support for object

--- src/main.cpp
+++ src/main.cpp
@@ -7,4 +7,8 @@ int main() {
     l.push_back(3);
     l.push_back("hello");
     std::cout << l[0] << ' ' << l[1] << '\n';
+    l[0] = "world";
+    l[1] = "50";
+    std::cout << (l[1].type() == algolib::type_id<std::string>()) << '\n';
+    std::cout << l[0] << ' ' << "2" + l[1]  << '\n';
 }
