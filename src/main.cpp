cd73abf5c576f845ec1fc26c869f31d52662ed90
Ali Hassan <aliwael01243@gmail.com>
2026-03-21 15:55:57 +0200

    Updated Python List

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "../include/data_structures/python_list.h"
 #include <iostream>
 #include <string>
+#include "../include/data_structures/vector.h"
 
 int main() {
     algolib::python_list l;
-    l.push_back(3);
-    l.push_back("hello");
-    std::cout << l[0] << ' ' << l[1] << '\n';
-    l[0] = "world";
-    l[1] = "50";
-    std::cout << (l[1].type() == algolib::type_id<std::string>()) << '\n';
-    std::cout << l[0] << ' ' << "2" + l[1]  << '\n';
+    using type = algolib::vector<int>;
+    l.push_back("zalata");
+    l.push_back(17);
+    l.push_back(type({4, 2006}));
+    l.pop_back();
+    std::cout << l[0] << ' ' << l[1] << ' '
+        << l.size();
 }
